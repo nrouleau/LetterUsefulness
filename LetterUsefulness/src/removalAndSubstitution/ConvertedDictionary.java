e9c7b7d7d3464360725b7a2c44f1cf317c5448c8
Noah <Noah@10.0.1.8>
2014-11-22 21:06:45 -0500

    Converted the simple IO to a ConvertedDictionary object meant to hold different experiments

---
+++ LetterUsefulness/src/removalAndSubstitution/ConvertedDictionary.java
@@ -0,0 +1,51 @@
+package removalAndSubstitution;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Hashtable;
+import java.util.Scanner;
+
+public class ConvertedDictionary {
+	Hashtable<String, Integer> words;
+	int conversionType;
+	char removalLetter;
+	char replaceLetter;
+	Scanner wordInput;
+	
+	// Will use constructor overloading for more options
+	public ConvertedDictionary(String filename, int conversionType) {//, char removalLetter) {
+		// Create a scanner from the filename
+		Scanner wordInput = null;
+		try {
+			wordInput = new Scanner(new File(filename));
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found: " + e.getMessage());
+			System.exit(0);
+		}
+		this.wordInput = wordInput;
+		this.conversionType = conversionType;
+		//this.removalLetter = removalLetter;
+		this.words = new Hashtable<String, Integer>();
+	}
+	
+	public void takeInput() {
+		int i = 0;
+		while ((wordInput.hasNextLine())&&(i < 100)) {
+			String nextWord = wordInput.nextLine();
+			System.out.println("line:"+nextWord+":");
+			// Increment existing matches
+			if (words.containsKey(nextWord)) { 
+				words.put(nextWord, new Integer(words.get(nextWord).intValue())); 
+			} else { // Place new entries otherwise
+				words.put(nextWord, new Integer(0));
+			}
+			++i;
+		}
+		if (words.isEmpty()) { 
+			System.out.println("Error: Hashtable is empty");
+			System.exit(0);
+		}
+		wordInput.close();
+	}
+	
+}
--- LetterUsefulness/src/removalAndSubstitution/TestRunner.java
+++ LetterUsefulness/src/removalAndSubstitution/TestRunner.java
@@ -1,45 +1,12 @@
 package removalAndSubstitution;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Hashtable;
-import java.util.Scanner;
-
 public class TestRunner {
 
 	public static void main(String[] args) {
 		System.out.println("Hello, world!");
-		Scanner wordInput = null;
-		try {
-			wordInput = new Scanner(new File("wordlist.txt" ));
-		} catch (FileNotFoundException e) {
-			System.out.println("File not found: " + e.getMessage());
-			System.exit(0);
-		}
 		
-		// Keys will be words in the English language in their converted form, 
-		// values will be number of matches between converted words
-		Hashtable<String, Integer> wordMatching = new Hashtable<String, Integer>();
-		int i = 0;
-		while ((wordInput.hasNextLine())&&(i < 100)) {
-			String nextWord = wordInput.nextLine();
-			System.out.println("line:"+nextWord+":");
-			// Increment existing matches
-			if (wordMatching.containsKey(nextWord)) { 
-				wordMatching.put(nextWord, new Integer(wordMatching.get(nextWord).intValue())); 
-			} else { // Place new entries otherwise
-				wordMatching.put(nextWord, new Integer(0));
-			}
-			++i;
-		}
-		if (wordMatching.isEmpty()) { 
-			System.out.println("Error: Hashtable is empty");
-			System.exit(0);
-		}
-		
-		
-		
-		wordInput.close();
+		ConvertedDictionary simpleDictionary = new ConvertedDictionary("wordlist.txt", 0);
+		simpleDictionary.takeInput();
 	}
 
 }
