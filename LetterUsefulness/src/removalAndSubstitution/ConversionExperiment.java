4ea3d0e0e6415399d4397616ea30ad9cdff110ff
Noah <Noah@10.0.1.8>
2014-11-22 21:58:51 -0500

    Created ConversionExperiment object to hold the data generated from a ConvertedDictionary experiment.

---
+++ LetterUsefulness/src/removalAndSubstitution/ConversionExperiment.java
@@ -0,0 +1,87 @@
+package removalAndSubstitution;
+
+/**
+ * Basically just a C struct - holds data about a particular ConvertedDictionary
+ * @author Noah
+ *
+ */
+public class ConversionExperiment {
+	private int totalWords; // The total number of input words
+	private int totalUniqueConvertedWords; // The number of unique words after conversion
+	private int sumDuplicates; // The number of duplicate converted words
+	private double duplicatesPerWord; // The number of duplicates per unconverted word
+	private double duplicatesPerConvertedWord; // The number of duplicates per converted word
+	private double varianceOfDuplication; // The variance of the number of duplicates in the converted words
+	
+	/*
+	 * Constructor
+	 */
+	public ConversionExperiment() {
+		// Default Values
+		totalWords = 0;
+		totalUniqueConvertedWords = 0;
+		sumDuplicates = 0;
+		duplicatesPerWord = 0;
+		duplicatesPerConvertedWord = 0;
+		varianceOfDuplication = Double.POSITIVE_INFINITY;
+	}
+	
+	/*
+	 * Getters
+	 */
+	public int getTotalWords() {
+		return totalWords;
+	}
+	public int getTotalUniqueConvertedWords() {
+		return totalUniqueConvertedWords;
+	}
+	public int getSumDuplicates() {
+		return sumDuplicates;
+	}
+	public double getDuplicatesPerWord() {
+		return duplicatesPerWord;
+	}
+	public double getDuplicatesPerConvertedWord() {
+		return duplicatesPerConvertedWord;
+	}
+	public double getVarianceOfDuplication() {
+		return varianceOfDuplication;
+	}
+	
+	/*
+	 * Setters
+	 */
+	public void setTotalWords(int totalWords) {
+		this.totalWords = totalWords;
+	}
+	public void setTotalUniqueConvertedWords(int totalUniqueConvertedWords) {
+		this.totalUniqueConvertedWords = totalUniqueConvertedWords;
+	}
+	public void setSumDuplicates(int sumDuplicates) {
+		this.sumDuplicates = sumDuplicates;
+	}
+	public void setDuplicatesPerWord(double duplicatesPerWord) {
+		this.duplicatesPerWord = duplicatesPerWord;
+	}
+	public void setDuplicatesPerConvertedWord(double duplicatesPerConvertedWord) {
+		this.duplicatesPerConvertedWord = duplicatesPerConvertedWord;
+	}
+	public void setVarianceOfDuplication(double varianceOfDuplication) {
+		this.varianceOfDuplication = varianceOfDuplication;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
